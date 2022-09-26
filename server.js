aff23577ebc5c9a37f6b0f8e76692878a10beb87
ssankaran <mailtonivass@gmail.com>
2022-09-26 15:31:09 +0530

    Create grpc server

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const config = require('config');
 const bodyParser = require("body-parser");
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+
 const app = express();
+const server = new grpc.Server();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(),
+(err, port)=>{
+    if (err != null) {
+        return console.error(err);
+    }
+    // start grpc server
+    server.start();
+    console.log(`gRPC listening on : ${port}`);
+});
 
 app.listen(config.port, () => {
   console.log("Express server listening on:", config.port);
