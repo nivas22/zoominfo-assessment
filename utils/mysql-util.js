79927990978271de47babec8f5859c566d9d4ab3
Seenivasan <mailtonivass@gmail.com>
2022-09-28 13:33:07 +0530

    Update docker setup

--- utils/mysql-util.js
+++ utils/mysql-util.js
@@ -3,7 +3,22 @@ const config = require('config');
 
 let pool;
 
-function createConnection(options) {
+function createConnection() {
+  console.log('process env', process.env);
+  let options = undefined
+  if(process.env.DB_HOST) {
+    options = {
+      "host": process.env.DB_HOST,
+      "user": process.env.DB_USER,
+      "password": process.env.DB_PASSWORD,
+      "port": process.env.DB_PORT,
+      "database": process.env.DB_NAME,
+      "connectionLimit": 200,
+      "multipleStatements": true
+    };
+  }
+  
+  console.log('options-------------', options);
   const mysqlOptions = options || config.mysql;
 
   mysqlOptions.typeCast = function (field, next) {
