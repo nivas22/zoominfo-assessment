dc4d1f5738df4ff789ac38cdaf103969643e3561
ssankaran <mailtonivass@gmail.com>
2022-09-28 23:09:48 +0530

    Update code

--- db/init.sql
+++ db/init.sql
@@ -19,6 +19,10 @@
 -- Table structure for table `file_content`
 --
 
+CREATE DATABASE filemanagement;
+
+use filemanagement;
+
 DROP TABLE IF EXISTS `file_content`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
